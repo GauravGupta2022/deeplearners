6b1107df8de736f0ce3c27e0f0b7031db9775bcc
GauravGupta2022 <ggaurav1919@gmail.com>
2025-07-28 14:25:44 -0700

    reorganized dataframes to be in separate Python file

---
+++ notebooks/df_stored.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+country_df = pd.read_csv('../data/raw/Country.csv')
+league_df = pd.read_csv('../data/raw/League.csv')
+match_df = pd.read_csv('../data/raw/Match.csv')
+player_attr_df = pd.read_csv('../data/raw/Player_Attributes.csv')
+player_df = pd.read_csv('../data/raw/Player.csv')
+sqlite_df = pd.read_csv('../data/raw/sqlite_sequence.csv')
+team_attr_df = pd.read_csv('../data/raw/Team_Attributes.csv')
+team_df = pd.read_csv('../data/raw/Team.csv')
