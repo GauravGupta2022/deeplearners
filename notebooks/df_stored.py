4eba5ebee55528570bcd1f7abaa38903a9278e1f
GauravGupta2022 <ggaurav1919@gmail.com>
2025-07-29 14:29:40 -0700

    adding other files

--- notebooks/df_stored.py
+++ notebooks/df_stored.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Raw data
 country_df = pd.read_csv('../data/raw/Country.csv')
 league_df = pd.read_csv('../data/raw/League.csv')
 match_df = pd.read_csv('../data/raw/Match.csv')
@@ -8,3 +9,6 @@ player_df = pd.read_csv('../data/raw/Player.csv')
 sqlite_df = pd.read_csv('../data/raw/sqlite_sequence.csv')
 team_attr_df = pd.read_csv('../data/raw/Team_Attributes.csv')
 team_df = pd.read_csv('../data/raw/Team.csv')
+
+# Cleaned data
+player_cleaned_df = pd.read_csv('../data/cleaned/player_attributes_cleaned.csv')
